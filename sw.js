6b02d4f71652cff47a28a2e4c5972dca0f77e5dc
adambajer <adam.bajer@gmail.com>
2025-05-07 23:19:08 +0200

    f

--- sw.js
+++ sw.js
@@ -1,52 +1,34 @@
 const CACHE_NAME = 'ssa-cache-v1';
 const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './style.css',
-  './site.webmanifest', // aktualizováno podle HTML
-  './favicon.ico',      // dle linků ve stránce
-  './icon.svg'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json',
+  '/fav.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    for (const url of FILES_TO_CACHE) {
-      try {
-        await cache.add(url);
-        console.log('[SW] Cached', url);
-      } catch (err) {
-        console.error('[SW] Cache failed for', url, err);
-      }
-    }
-  })());
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(
-      keys.map(key => key !== CACHE_NAME && caches.delete(key))
-    );
-    console.log('[SW] Activated, cleaned old caches');
-  })());
-  self.clients.claim();
+self.addEventListener('activate', evt => {
+  // Odstranění starých cache
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    const cachedResponse = await caches.match(event.request);
-    if (cachedResponse) {
-      return cachedResponse;
-    }
-    try {
-      const networkResponse = await fetch(event.request);
-      // volitelně: cache new requests
-      return networkResponse;
-    } catch (err) {
-      console.error('[SW] Fetch failed:', event.request.url, err);
-      throw err;
-    }
-  })());
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request)
+      .then(cachedResp => cachedResp || fetch(evt.request))
+  );
 });
